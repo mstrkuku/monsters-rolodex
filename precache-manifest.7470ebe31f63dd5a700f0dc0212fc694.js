cbefece826b58cc38a23e50ebffedfcfa93be558
mstrkuku <mysterkuku@gmail.com>
2020-04-18 01:12:44 -0400

    Updates

---
+++ precache-manifest.7470ebe31f63dd5a700f0dc0212fc694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1fdff58199b73fa3e6202c22d65116",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4bcefddd7107e4ab0912",
+    "url": "/monsters-rolodex/static/css/main.a95f5f34.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bcefddd7107e4ab0912",
+    "url": "/monsters-rolodex/static/js/main.0d35d034.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
